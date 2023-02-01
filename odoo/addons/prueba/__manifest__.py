d447f0d8e1872f6b842f43ba7b8c9a9698f0ac3f
Juanfh11 <juanfh2003@gmail.com>
2023-02-01 10:20:06 +0100

    New models and view

---
+++ odoo/addons/prueba/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "prueba",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Prueba de desarrolllo de un módulo de Odoo para poder hacer el trabajo de la asignatura de SGE
+    """,
+
+    'author': "Juan Fernández Hinojar",
+    'website': "http://www.reservahoteles.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'application': True
+}
---
+++ odoo/addons/prueba/models/models.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class PruebaCategoria(models.Model):
+    _name = 'jf.prueba.categoria'
+    _description = 'jf.prueba.categoria'
+    nombre = fields.Char(string='Nombre',required=True,help='Introduce el nombre de la categoria')
+    descripcion = fields.Text(string='Descripcion')
+
+
+class PruebaLibro(models.Model):
+    _name = 'jf.prueba.libro'
+    nombre = fields.Char(string='Título',required=True,help='Introduce el nombre del libro')
+    precio = fields.Float(string='Precio')
+    ejemplares = fields.Integer(string='ejemplares')
+    fecha = fields.Date(string='Fecha de compra')
+    segmano = fields.Boolean(string='Segunda mano')
+    estado = fields.Selection([
+        ('0','Bueno'),('1','Regular'),('2','Malo')
+    ], string='Estado',default="0")
+
+
+
+
+# class prueba(models.Model):
+#     _name = 'prueba.prueba'
+#     _description = 'prueba.prueba'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
