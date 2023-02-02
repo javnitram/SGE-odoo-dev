ef93f22db59a02cde4a78923a14def8ec9e527b2
cesarmh5 <merinohernandezcesar@gmail.com>
2023-02-02 10:20:14 +0100

    commit

--- odoo/addons/cm_veterinaria/models/models.py
+++ odoo/addons/cm_veterinaria/models/models.py
@@ -10,27 +10,27 @@ class VeterinariaMascotas(models.Model):
     race = fields.Char('Raza',required=True)
     owner = fields.Char('Owner')
 
-class MascotasVacunas(models.Model):
-    _name = 'cm.mascotas.vacunas'
-    _description = 'mascotas.vacunas'
-    vacuna = fields.Char('Vacuna', required=True)
-    field_name = fields.Selection([
-        ('0', 'Si'),
-        ('1', 'Pendiente')
-    ], string='Vacunado',required=True)
-    #field_name_ids = fields.Many2many('comodel_name', string='field_name')
+#class MascotasVacunas(models.Model):
+  #  _name = 'cm.mascotas.vacunas'
+  #  _description = 'mascotas.vacunas'
+  #  vacuna = fields.Char('Vacuna', required=True)
+  #  field_name = fields.Selection([
+   #     ('0', 'Si'),
+  #      ('1', 'Pendiente')
+   # ], string='Vacunado',required=True)
+   # #field_name_ids = fields.Many2many('comodel_name', string='field_name')
 
-class VeterinariaVeterinario(models.Model):
-    _name = 'cm.veterinaria.veterinario'
-    _description = 'veterinaria.veterinario'
-    nombre = fields.Char('Nombre',required=True)
-    numemp = fields.Char('Num Empleado',required=True)
+#class VeterinariaVeterinario(models.Model):
+  #  _name = 'cm.veterinaria.veterinario'
+  #  _description = 'veterinaria.veterinario'
+  #  nombre = fields.Char('Nombre',required=True)
+  #  numemp = fields.Char('Num Empleado',required=True)
 
-class VeterinariaCliente(models.Model):
-    _name = 'cm.veterinaria.cliente'
-    _description = 'veterinaria.cliente'
-    name = fields.Char('Nombre',required=True)
-    field_name_ids = fields.One2many('comodel_name', 'inverse_field_name', string='field_name')
+#class VeterinariaCliente(models.Model):
+ #   _name = 'cm.veterinaria.cliente'
+  #  _description = 'veterinaria.cliente'
+  #  name = fields.Char('Nombre',required=True)
+   # field_name_ids = fields.One2many('comodel_name', 'inverse_field_name', string='field_name')
 
 # class cm_veterinaria(models.Model):
 #     _name = 'cm_veterinaria.cm_veterinaria'
