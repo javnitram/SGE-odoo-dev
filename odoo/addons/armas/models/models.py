efe9287b2ebfe2fbaa8ff41cd7c51dca73e6ae70
KaibaKudo <adrian.cortes.4869@gmail.com>
2023-02-03 12:03:34 +0100

    Modelos y vistas definidos

--- odoo/addons/armas/models/models.py
+++ odoo/addons/armas/models/models.py
@@ -16,3 +16,51 @@
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
+from odoo import models,api,fields
+
+
+class Armas(models.Model):
+    _name = 'ac.armas.categoria'
+    _description = 'Armas'
+    
+    nombre_arma = fields.Char('nombre_arma')
+    precio = fields.Float('precio')
+    referencia = fields.Char('referencia')
+    categoria = fields.Selection([
+        ('1', 'Fusil de asalto'),('2', 'Subfusil'),('3','Escopeta'),('4','Francotirador'),('5','Lanzacohetes')
+    ], string='categoria')
+    disponibilidad = fields.Boolean('disponobilidad')
+
+
+class accesoriosArmas(models.Model):
+    _name = 'ac.accesorios.armas'
+    _description = 'AccesoriosArmas'
+
+    nombre_accesorio = fields.Char('nombre_accesorio')
+    precio = fields.Float('precio')
+    referencia = fields.Char('referencia')
+    disponibilidad = fields.Boolean('disponobilidad')
+
+
+class camuflajesArmas(models.Model):
+    _name = 'ac.camuflajes.armas'
+    _description = 'CamuflajesArmas'
+
+    nombre_camuflaje = fields.Char('nombre_camuflaje')
+    precio = fields.Float('precio')
+    referencia = fields.Char('referencia')
+    disponibilidad = fields.Boolean('disponobilidad')
+
+
+class clientes(models.Model):
+    _name = 'ac.clientes.tienda'
+    _description = 'Clientes'
+
+    nombre_cliente = fields.Char('nombre_cliente')
+    telefono = fields.Float('telefono')
+    fecha_nacimiento = fields.Date('fecha_nacimiento')
+    direccion = fields.Char('direccion')
+        
+        
+        
