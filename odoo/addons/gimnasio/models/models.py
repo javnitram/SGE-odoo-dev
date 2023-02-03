454c50a13ff689958ad6aa61fcde5213d657dff5
Berto99 <albertonaval@live.com>
2023-02-03 12:01:47 +0100

    Model,view,model_access configuracion basica

--- odoo/addons/gimnasio/models/models.py
+++ odoo/addons/gimnasio/models/models.py
@@ -16,3 +16,46 @@
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
+
+from odoo import models,api,fields
+
+
+class ClientesGimnasio(models.Model):
+    _name = 'ar.gimnasio.clientes'
+    _description = 'ar.gimnasio.clientes'
+
+    dni = fields.Char('DNI',required=True)
+    nombre = fields.Char('Nombre',required=True)
+    activo = fields.Boolean('Activo?',default=True)
+    telefono = fields.Integer('telefono',required=True)
+
+class ClasesGimnasio(models.Model):
+    _name = 'ar.gimnasio.clases'
+    _description = 'ar.gimnasio.clases'
+    
+    nombre_clase = fields.Char('nombre_clase',required=True)
+    precio = fields.Float('precio',required=True)
+    monitor = fields.Char('monitor',required=True)
+    
+class InscripcionesGimnasio(models.Model):
+    _name = 'ar.gimnasio.inscripciones'
+    _description = 'ar.gimnasio.inscripciones'
+
+    tipo_inscripcion = fields.Selection([
+        ('0', '1 mes'),
+        ('1', '3 meses'),
+        ('2', '6 meses'),
+        ('3', '12 meses')
+    ],string='tipo_inscripcion')
+    fecha_inscripcion = fields.Date('fecha_inscripcion',required=True)
+    precio = fields.Float('precio')
+
+class ProductosGimnasio(models.Model):
+    _name = 'ar.gimnasio.productos'
+    _description = 'ar.gimnasio.productos'
+
+    nombre_producto = fields.Char('nombre_producto',required=True)
+    precio = fields.Float('precio',required=True)
+    disponibilidad = fields.Boolean('disponibilidad',default=True)
+    descripcion = fields.Char('descripcion')
