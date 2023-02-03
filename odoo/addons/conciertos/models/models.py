815042eb4e2273109e9079847d7f8723790387a3
amsamu <samaropulido@gmail.com>
2023-02-03 12:49:39 +0100

    Updated models and views

--- odoo/addons/conciertos/models/models.py
+++ odoo/addons/conciertos/models/models.py
@@ -11,14 +11,24 @@ class ConciertoRecinto(models.Model):
     _name = 'sa.conciertos.recinto'
     _description = 'sa.conciertos.recinto'
     nombre = fields.Char('nombre')
+    ciudad = fields.Char('ciudad')
+    país = fields.Selection([
+        ('key', 'value')
+    ], string='país')
+    capacidad = fields.Integer('capacidad')
 
 class ConciertosConcierto(models.Model):
     _name = 'sa.conciertos.concierto'
     _description = 'sa.conciertos.concierto'
+    fecha = fields.Date('fecha')
+    artista_id = fields.Many2one('sa.conciertos.artista', string='artista')
+    recinto_id = fields.Many2one('sa.conciertos.recinto', string='recinto')
+    
 
 class ConciertoEntrada(models.Model):
     _name = 'sa.conciertos.entrada'
     _description = 'sa.conciertos.entrada'
+    precio = fields.Float('precio')
 
 
 # class conciertos(models.Model):
