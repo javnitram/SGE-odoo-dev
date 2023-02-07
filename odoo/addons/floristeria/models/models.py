57e3b4691b7abaa2f0e11e78fd918aae58acacf8
fdezhector <hectorfernandezruiz.02@gmail.com>
2023-02-07 14:17:45 +0100

    updated models

--- odoo/addons/floristeria/models/models.py
+++ odoo/addons/floristeria/models/models.py
@@ -9,9 +9,24 @@ class floristeria(models.Model):
 class flower(models.Model):
     _name = 'hf_floristeria_flower'
     _description = 'Flor'
-    name = fields.Char('Flor')
+    name = fields.Char(string='Nombre común',required=True, help='')
+    scientific_name = fields.Char(string='Nombre científico', help='')
+    specie = fields.Char(string='Especie', help='')
+    main_color = fields.Char(string='Color predominante', help='')
+    """ group = fields.Selection([
+        ('A', 'Angiospermas'),
+        ('G', 'Gimnospermas')
+    ], string='Grupo',required=True)
+    subgroup = fields.Selection([
+        ('0', 'Cícadas'),
+        ('1', 'Ginko'),
+        ('2', 'Coníferas'),
+        ('3', 'Gnetales')
+    ], string='Subgrupo',required=True) """
 
 class bouquet(models.Model):
     _name = 'hf_floristeria_bouquet'
     _description = 'Ramo de flores'
-    
+    size = fields.Char('Tamaño', required=True)
+    price = fields.Float('Precio', required=True)
+    delivery_date = fields.Date('Fecha de entrega')
