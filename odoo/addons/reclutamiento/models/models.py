3c7772e57c8f33730d79f1d77fc60559a656fb13
canita402 <alvaromaker402@gmail.com>
2023-02-08 10:19:29 +0100

    commitMal1

--- odoo/addons/reclutamiento/models/models.py
+++ odoo/addons/reclutamiento/models/models.py
@@ -6,14 +6,24 @@ from odoo import models, fields, api
 class ejercito(models.Model):
     _name = 'ac.reclutamiento.ejercito'
     _description = 'ac.reclutamiento.ejercito'
-    id_ejercito= fields.Integer()
+    id_ejercito= fields.Integer(required=True)
     nombre = fields.Char()
+    unidades= fields.Integer()
+
+class general(models.Model):
+    _name = 'ac.reclutamiento.general'
+    _description = 'ac.reclutamiento.general'
+    id_general= fields.Integer(required=True)
+    nombre = fields.Char()
+    edad= fields.Char() 
+  
   
 
 
 class infanteria_CaC(models.Model):
     _name = 'ac.reclutamiento.infanteria.cac'
     _description = 'ac.reclutamiento.infanteria.cac'
+    id_cac= fields.Integer(required=True) 
     nombre = fields.Char()
     descripcion = fields.Text()
     armadura = fields.Selection([
@@ -25,15 +35,18 @@ class infanteria_CaC(models.Model):
 class infanteria_proyectiles(models.Model):
     _name = 'ac.reclutamiento.infanteria.proyectiles'
     _description = 'ac.reclutamiento.infanteria.proyectiles'
+    id_proyectiles=fields.Integer(required=True) 
     nombre = fields.Char()
     descripcion = fields.Text()
     municion= fields.Integer()
     ##poner valor por defecto
     alcance= fields.Integer()
 
+
 class undiades_motadas(models.Model):
     _name = 'ac.reclutamiento.unidades.montadas'
     _description = 'ac.reclutamiento.montadas'
+    id_montadas= fields.Integer(required=True) 
     nombre = fields.Char()
     descripcion = fields.Text()
     monturas = fields.Selection([
@@ -45,6 +58,7 @@ class undiades_motadas(models.Model):
 class artilleria(models.Model):
     _name = 'ac.reclutamiento.artilleria'
     _description = 'ac.reclutamiento.artilleria'
+    id_artilleria= fields.Integer(required=True) 
     nombre = fields.Char()
     descripcion = fields.Text()
     municion = fields.Selection([
