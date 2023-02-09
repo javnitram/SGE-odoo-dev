51dc4910cd8b9e78af20d37c4cb4fd39bd5f0734
sanbricio <sanbriciorojas11@gmail.com>
2023-02-09 10:24:48 +0100

    nuevos forms

--- odoo/addons/game_shop/__manifest__.py
+++ odoo/addons/game_shop/__manifest__.py
@@ -10,13 +10,13 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Santiago Bricio Rojas",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'module_category_sales',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
