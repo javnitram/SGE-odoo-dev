3d19463d8969aceea1c9fabdf488a0ce99d8f34f
sanbricio <sanbriciorojas11@gmail.com>
2023-02-09 13:55:19 +0100

    Distribuidor y bug nombre solucionado

--- odoo/addons/game_shop/models/models.py
+++ odoo/addons/game_shop/models/models.py
@@ -6,7 +6,7 @@ from odoo import models, fields, api
 class game_shop_tienda(models.Model):
     _name = 'game_shop.tienda'
     _description = 'Tienda de videojuegos'
-    nombre= fields.Char(string="Nombre",required=True,help="Introduce el nombre de la tienda")
+    name= fields.Char(string="Nombre",required=True,help="Introduce el nombre de la tienda")
     ubicacion = fields.Char(string="Ubicación",required=True,help="Introduce la ubicación de la tienda")
     horario = fields.Char(string ="Horario",help="Introduce el horario de la tienda",default="10:00")
     
@@ -14,18 +14,19 @@ class game_shop_tienda(models.Model):
 class game_shop_videojuego(models.Model):
     _name = 'game_shop.videojuego'
     _description = 'Videojuego'
-    nombre = fields.Char(string="Nombre",required=True,help="Introduce el nombre de el videojuego")
+    name = fields.Char(string="Nombre",required=True,help="Introduce el nombre de el videojuego")
     pegi= fields.Integer(string="Pegi",required=True,default=3,help="Introduce la edad minima para poder jugar al juego")
     precio = fields.Float(string="Precio",required=True,help="Introduce el precio del videojuego")    
     digital = fields.Boolean(string="Digital")
     estado = fields.Selection([
         ('0', 'Nuevo'),('1','Seminuevo'),('2','Usado')
     ], string='Estado', required=True,default="0")
+    imagen = fields.Image(string="Imagen", max_width=640, max_height=480)
 
 class game_shop_distribuidor(models.Model):
     _name = 'game_shop.distribuidor'
     _description = 'Distribuidor'
-    nombre = fields.Char(string="Nombre", required=True,help="Introduce el nombre del distribuidor")
+    name = fields.Char(string="Nombre", required=True,help="Introduce el nombre del distribuidor")
     sede = fields.Selection([
         ('0', 'Sevilla'),('1','Barcelona'),('2','Madrid'),
         ('3','Valencia'),('4','Santiago de Compostela'),('5','Valladolid'),
@@ -38,7 +39,7 @@ class game_shop_distribuidor(models.Model):
 class game_shop_almacen(models.Model):
     _name = 'game_shop.almacen'
     _description = 'Almacen juegos'
-    nombre = fields.Char(string = "Almacen",required=True,help="Introduce el nombre del almacen",default="almacen")
+    name = fields.Char(string = "Almacen",required=True,help="Introduce el nombre del almacen",default="almacen")
     cantidad = fields.Integer(string = "Cantidad", help="Cantidad de juegos en el almacen",default=0)
     #fk juego
 
