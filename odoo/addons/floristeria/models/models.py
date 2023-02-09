3885744ebd99b14cf7035cab992f89006a5e13b8
fdezhector <hectorfernandezruiz.02@gmail.com>
2023-02-09 10:28:28 +0100

    clases más detalladas

--- odoo/addons/floristeria/models/models.py
+++ odoo/addons/floristeria/models/models.py
@@ -11,10 +11,11 @@ class flower(models.Model):
     _name = 'hf_floristeria_flower'
     _description = 'Flor'
     name = fields.Char(string='Nombre común',required=True, help='')
-    scientific_name = fields.Char(string='Nombre científico', help='')
-    specie = fields.Char(string='Especie', help='')
+    scientific_name = fields.Char(string='Nombre científico', required=True, help='')
+    specie = fields.Char(string='Especie', required=True, help='')
     main_color = fields.Char(string='Color predominante', help='')
     group = fields.Selection([
+        ('0', ''),
         ('A', 'Angiospermas'),
         ('G', 'Gimnospermas')
     ], string='Grupo',required=True)
@@ -24,11 +25,19 @@ class flower(models.Model):
         ('2', 'Coníferas'),
         ('3', 'Gnetales')
     ], string='Subgrupo',required=True) """
+    image = fields.Image('Imagen')
 
 class bouquet(models.Model):
     _name = 'hf_floristeria_bouquet'
     _description = 'Ramo de flores'
-    name = fields.Char('Nombre')
-    size = fields.Char('Tamaño', required=True)
+    name = fields.Char('Nombre', required=True)
+    size = fields.Selection([
+        ('0', ''),
+        ('1', 'Pequeño'),
+        ('2', 'Normal'),
+        ('3', 'Grande'),
+        ('4', 'Enorme')
+    ], string='Tamaño',required=True)
     price = fields.Float('Precio', required=True)
     delivery_date = fields.Date('Fecha de entrega')
+    image = fields.Image('Imagen')
