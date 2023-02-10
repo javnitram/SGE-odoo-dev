11607cbf1da715d1a2a9969d07be56632580c6eb
Daebore <javier_m_h_1993@hotmail.com>
2023-02-10 12:16:24 +0100

    vistas terminadas y permisos dados

--- odoo/addons/taberna/models/models.py
+++ odoo/addons/taberna/models/models.py
@@ -6,55 +6,54 @@ from odoo import models, fields, api
 class TabernaInformacion(models.Model):
     _name = 'jm.taberna.informacion'
     _description = 'jm.taberna.informacion'
-    nombre = fields.Char('nombre', required=True)
-    direccion = fields.Char('direccion')
+    nombre = fields.Char('Nombre', required=True)
+    direccion = fields.Char('Dirección')
 
 class TabernaBebidas(models.Model):
     _name = 'jm.taberna.bebidas'
     _description = 'jm.taberna.bebidas'
-    id_bebida = fields.Integer('id', required=True)
-    nombre = fields.Char('nombre')
-    alcohol = fields.Boolean('alcohol')
-    tipo = fields.Char('tipo')
-    pais = fields.Char('pais')
+    id_bebida = fields.Integer('ID', required=True)
+    nombre = fields.Char('Nombre')
+    alcohol = fields.Boolean('Alcohol')
+    pais = fields.Char('Pais')
     tipo = fields.Selection([
-        ('1', 'rubia'),
-        ('2', 'tostada'),
-        ('3', 'negra')
-    ], string='tipo')
+        ('1', 'Rubia'),
+        ('2', 'Tostada'),
+        ('3', 'Negra')
+    ], string='Tipo')
     id_pedidos = fields.Many2many('jm.taberna.pedidos', string='id_pedidos')
 
 class TabernaEmpleados(models.Model):
     _name = 'jm.taberna.empleados'
     _description = 'jm.taberna.empleados'
-    id_empleado = fields.Integer('id', required=True)
-    nombre = fields.Char('nombre')
-    apellido1 = fields.Char('apellido1')
-    apellido2 = fields.Char('apellido2')
-    salario = fields.Float('salario')
-    telefono = fields.Integer('telefono')
+    id_empleado = fields.Integer('ID', required=True)
+    nombre = fields.Char('Nombre')
+    apellido1 = fields.Char('Apellido 1')
+    apellido2 = fields.Char('Apellido 2')
+    salario = fields.Float('Salario')
+    telefono = fields.Integer('Teléfono')
     categoria = fields.Selection([
-        ('1', 'camarero'),
-        ('2', 'cocinero'),
-    ], string='categoria')
-    nombre_taberna = fields.Many2one('jm.taberna.informacion', string='nombre_taberna')
+        ('1', 'Camarero'),
+        ('2', 'Cocinero'),
+    ], string='Categoría')
+    nombre_taberna = fields.Many2one('jm.taberna.informacion', string='Nombre de la taberna')
 
 class TabernaClientes(models.Model):
     _name = 'jm.taberna.clientes'
     _description = 'jm.taberna.clientes'
-    id_cliente= fields.Integer('id', required=True)
-    nombre = fields.Char('nombre')
-    apellido1 = fields.Char('apellido1')
-    apellido2 = fields.Char('apellido2')
+    id_cliente= fields.Integer('ID', required=True)
+    nombre = fields.Char('Nombre')
+    apellido1 = fields.Char('Apellido 1')
+    apellido2 = fields.Char('Apellido 2')
 
 class TabernaPedidos(models.Model):
     _name = 'jm.taberna.pedidos'
     _description = 'jm.taberna.pedidos'
-    id_pedidos = fields.Integer('id', required=True)
-    cantidad = fields.Integer('cantidad')
-    id_empleado = fields.Many2one('jm.taberna.empleados', string='id_empleado')
-    id_cliente = fields.Many2one('jm.taberna.clientes', string='id_cliente')
-    id_bebida = fields.Many2many('jm.taberna.bebidas', string='id_bebida')
+    id_pedidos = fields.Integer('ID', required=True)
+    cantidad = fields.Integer('Cantidad')
+    id_empleado = fields.Many2one('jm.taberna.empleados', string='ID de empleado')
+    id_cliente = fields.Many2one('jm.taberna.clientes', string='ID de cliente')
+    id_bebida = fields.Many2many('jm.taberna.bebidas', string='ID de bebida')
 
 # classpaisuired
 # salario = fields.Float('salario')=Truern, re, requ, required=Trueired=Trueq
