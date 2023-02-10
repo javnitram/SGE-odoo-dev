eabadb7263f47997eefcdf4572845d509901cec4
amsamu <samaropulido@gmail.com>
2023-02-10 12:55:15 +0100

    Small changes on models

--- odoo/addons/conciertos/models/models.py
+++ odoo/addons/conciertos/models/models.py
@@ -5,31 +5,31 @@ from odoo import models, fields, api
 class ConciertosArtista(models.Model):
     _name = 'sa.conciertos.artista'
     _description = 'sa.conciertos.artista'
-    nombre = fields.Char('nombre', required=True)
+    nombre = fields.Char('Nombre', required=True)
 
 class ConciertoRecinto(models.Model):
     _name = 'sa.conciertos.recinto'
     _description = 'sa.conciertos.recinto'
-    nombre = fields.Char('nombre')
-    ciudad = fields.Char('ciudad')
-    país = fields.Selection([
+    nombre = fields.Char('Nombre')
+    ciudad = fields.Char('Ciudad')
+    pais = fields.Selection([
         ('key', 'value')
-    ], string='país')
-    capacidad = fields.Integer('capacidad')
+    ], string='País')
+    capacidad = fields.Integer('Capacidad')
 
 class ConciertosConcierto(models.Model):
     _name = 'sa.conciertos.concierto'
     _description = 'sa.conciertos.concierto'
-    fecha = fields.Date('fecha')
-    artista_id = fields.Many2one('sa.conciertos.artista', string='artista')
-    recinto_id = fields.Many2one('sa.conciertos.recinto', string='recinto')
+    fecha = fields.Date('Fecha')
+    artista_id = fields.Many2one('sa.conciertos.artista', string='Artista')
+    recinto_id = fields.Many2one('sa.conciertos.recinto', string='Recinto', help='El recinto donde tendrá lugar')
     
 
 class ConciertoEntrada(models.Model):
     _name = 'sa.conciertos.entrada'
     _description = 'sa.conciertos.entrada'
-    precio = fields.Float('precio')
-
+    concierto_id = fields.Many2one('sa.conciertos.concierto', string='Concierto')
+    precio = fields.Float('Precio')
 
 # class conciertos(models.Model):
 #     _name = 'conciertos.conciertos'
