a24672ad8e9b5064922110fdedd2de3456050f66
Gilzan <gilandrew115@gmail.com>
2023-02-13 14:15:07 +0100

    Relaciones de libreria

--- odoo/addons/libreria/models/models.py
+++ odoo/addons/libreria/models/models.py
@@ -6,6 +6,7 @@ class LibreriaCategoria(models.Model):
     _name = "libreria.categoria"
     name = fields.Char(string="Nombre",required=True,help="Introduce el nombre de la categoria")
     description = fields.Text(string="Descripción")
+    libros_ids = fields.One2many('libreria.libro', 'categoria_id', string='Libros de la categoría')
 
 class LibreriaLibro(models.Model):
     _name = "libreria.libro"
@@ -15,4 +16,11 @@ class LibreriaLibro(models.Model):
     fecha = fields.Date(string="Fecha de compra")
     segmano = fields.Boolean(string="Segunda mano")
     estado = fields.Selection([('0','Bueno'),('1','Regular'),('2','Malo')],string="Estado",default="0")
+    categoria_id = fields.Many2one('libreria.categoria', string='Categoría')
+    autores_ids = fields.Many2many('libreria.autor', string='Autores')
 
+class LibreriaAutor(models.Model):
+    _name="libreria.autor"
+    _description= "Modelo Autor"
+    name = fields.Char(string="Nombre",required=True,help="Introduce el nombre del autor")
+    libros_ids = fields.Many2many('libreria.libro', string='Libros de autores')
