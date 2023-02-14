7a33d4dc999ffdfc11c235be75e3598b87d5362e
Daviis <Daviisclase@gmail.com>
2023-02-14 19:36:22 +0100

    Added courts and match views, eliminated one model

--- odoo/addons/ooddle/models/models.py
+++ odoo/addons/ooddle/models/models.py
@@ -11,14 +11,11 @@ class Users(models.Model):
     league = fields.Char( 'league')
     mail = fields.Char( 'mail', required=True)
     image = fields.Image('image')
-
-class UserResgistered(models.Model):
-    _name = 'dm.ooddle.usersregistered'
-    user_registered = fields.Many2one('dm.ooddle.users','user_id', help= "Introduce el usuario que jugara el partido")
-    match = fields.Many2many('dm.ooddle.matches','match_id', help= 'Introduce el partido nuevo')
+    matches_ids = fields.Many2many('dm.ooddle.matches', string='matches')
 
 class Matches(models.Model):
     _name = 'dm.ooddle.matches'
+    users_ids = fields.Many2many('dm.ooddle.users', string='UserResgitered')
     court = fields.Many2one('dm.ooddle.courts','court_id',required=True)
     time = fields.Datetime('time', required=True, help= 'indica la fecha del partido')
     price = fields.Float('price', required=True)
@@ -27,5 +24,5 @@ class Courts(models.Model):
     _name = 'dm.ooddle.courts'
     match_ids = fields.One2many('dm.ooddle.matches', 'court', string='indica el partido que se jugara en esa vista')
     place = fields.Char('place', required=True, help = 'Introduce el lugar de la pista')
-    used = fields.Boolean( 'used', default = False, help = 'La pista esta en uso')
+    in_use = fields.Boolean( 'in_use', default = False, help = 'La pista esta en uso')
 
