d3f30389e31e68e33e0ff8e3aa16d10cb5c97857
deleseee <deleseee@gmail.com>
2023-02-14 13:39:31 +0100

    imagen añadida, cambios en modelo y vistas y algunas relacciones hechas

--- odoo/addons/concesionario/models/models.py
+++ odoo/addons/concesionario/models/models.py
@@ -6,12 +6,13 @@ from odoo import models, fields, api
 class ConcesionarioPedidos(models.Model):
     _name = 'dhm.concesionario.pedidos'
     _description = 'Marcas'
-    id = fields.Integer('Id',required=True)
+    nombre = fields.Char('Nombre',required=True)
+    descripcion = fields.Char('Descripcion')
+    vehiculos_ids = fields.One2many('dhm.concesionario.vehiculos', 'vehiculos_id', string='vehiculos')
 
 class ConcesionarioVehiculos(models.Model):
     _name = 'dhm.concesionario.vehiculos'
     _description = 'Vehiculo'
-    id = fields.Integer('Id',required=True)
     marca = fields.Char('Nombre',required=True,help='Introduzca el nombre de la marca')
     modelo = fields.Char('Nombre',required=True,help='Introduzca el nombre del modelo')
     estado = fields.Selection([
@@ -19,18 +20,20 @@ class ConcesionarioVehiculos(models.Model):
         ('1', 'KM-0'),
         ('2', 'Seminuevo')
     ], string='Estado')
+    matricula = fields.Integer('Matricula',required=True)
+    vehiculos_id = fields.Many2one('dhm.concesionario.vehiculos', string='vehiculos')
 
 class ConcesionarioClientes(models.Model):
     _name = 'dhm.concesionario.clientes'
     _description = 'Clientes'
     dni = fields.Integer('Dni',required=True)
     nombre = fields.Char('Nombre',required=True,help='Introduzca el nombre')
-    apellidos = fields.Char('apellidos',required=True,help='Introduzca el apellido')
+    apellidos = fields.Char('Apellidos',required=True,help='Introduzca el apellido')
 
 
-class ConcesionarioMatriculacion(models.Model):
-    _name = 'dhm.concesionario.matriculacion'
-    _description = 'Matriculacion'
-    matricula = fields.Integer('Matricula',required=True)
-    
+class ConcesionarioFacturas(models.Model):
+    _name = 'dhm.concesionario.facturas'
+    _description = 'Facturas'
+    cantidad = fields.Integer('Cantidad',required=True)
+    precio = fields.Integer('Precio',required=True)
     
