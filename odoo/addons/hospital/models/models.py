4574631715a0903ee12c704356b732bd644fb5fe
rot105 <gilandrew115@gmail.com>
2023-02-14 21:30:07 +0100

    Models, views, hospital

--- odoo/addons/hospital/models/models.py
+++ odoo/addons/hospital/models/models.py
@@ -3,33 +3,36 @@
 from odoo import models, fields, api
 
 class HospitalDoctores(models.Model):
-    _name="hospital.doctores"
+    _name="gs.hospital.doctores"
+    dni=fields.Char(string="DNI",required=True)
     name=fields.Char(string ="Nombre",required=True,help="Introduce el nombre")
     surname=fields.Char(string = "Apellido",required=True,help="Introduce el apellido")
     date=fields.Date(string="Fecha de nacimiento")
-    specialty=fields.Char(string = "Especialidad",required=True,help="Introduce la especialidad")
+    tlf=fields.Int(int="Teléfono")
+    especialidad=fields.Char(string = "Especialidad",required=True,help="Introduce la especialidad")
 
 class HospitalPacientes(models.Model):
-    _name="hospital.pacientes"
+    _name="gs.hospital.pacientes"
+    dni=fields.Char(string="DNI",required=True)
     name=fields.Char(string="Nombre",required=True,help="Introduce el nombre")
     surname=fields.Char(string="Apellido",required=True,help="Introduce el apellido")
     date=fields.Date(string="Fecha de nacimiento")
+    tlf=fields.Int(int="Teléfono")
     history=fields.Char(string="Historia",required=True,help="Introduce la razón de visita al hospital")
     alergias=fields.Char(string="Alergias",help="Posible alergia a medicamentos")
 
 class HospitalEnfermeros(models.Model):
-    _name="hospital.enfermeros"
+    _name="gs.hospital.enfermeros"
+    dni=fields.Char(string="DNI",required=True)
     name=fields.Char(string ="Nombre",required=True,help="Introduce el nombre")
     surname=fields.Char(string = "Apellido",required=True,help="Introduce el apellido")
     date=fields.Date(string="Fecha de nacimiento")
     specialty=fields.Char(string = "Especialidad",required=True,help="Introduce la especialidad")
 
-class HospitalTratamientos(models.Model):
-    _name="hospital.tratamientos"
+class HospitalMedicinas(models.Model):
+    _name="gs.hospital.medicinas"
+    nroserie=fields.Char(string="Número de serie",required=True)
     name=fields.Char(string="Nombre",required=True,help="Introduce el nombre")
-    surname=fields.Char(string="Apellido",required=True,help="Introduce el apellido")
-    date=fields.Date(string="Fecha de nacimiento")
-    history=fields.Char(string="Historia",required=True,help="Introduce la razón de visita al hospital")
-    alergias=fields.Char(string="Alergias",help="Posible alergia a medicamentos")
-
+    cantidad=fields.Int(int="Cantidad", required=True)
+    tipo=fields.Char(string="Tipo de medicamento")
 
