39b172d518cd0321a3183f9ea76422a449853c26
Arkidon <arkidon850@gmail.com>
2023-02-15 14:00:23 +0100

    Updated models

--- odoo/addons/computer_repair_shop/models/models.py
+++ odoo/addons/computer_repair_shop/models/models.py
@@ -2,8 +2,38 @@
 
 from odoo import models, fields
 
-class Repair(models.Model):
-    _name = 'ws.repair'
-    _description = 'Repair Model'
+class Technician(models.Model):
+    _name = "ws.technician"
+    _description = "Technician Model"
 
-    repair_status = fields.Integer('repair_status')
+    name = fields.Char('name')
+    photo = fields.Image('photo', max_width=512, max_height=512)
+
+
+class Repairment(models.Model):
+    _name = 'ws.repairment'
+    _description = 'Repair Model'
+    repair_status = fields.Selection([
+        ('quote', 'Presupuesto'),
+        ('accepted', "Aceptado"),
+        ('processing', "En proceso"),
+        ('ready', 'Listo')
+    ], string='repair_status')
+    start_date = fields.Date('start_date')
+    completion_date = fields.Date('completion_date')
+    labor_cost = fields.Float('labor_cost')
+
+
+class Replacement(models.Model):
+    _name = "ws.replacement"
+    _description = "Replacement Model"
+    old_part_id = fields.Many2one('comodel_name', string='old_part')
+    new_part_id = fields.Many2one('comodel_name', string='new_part')
+
+
+class Part(models.Model):
+    _name = "ws.part"
+    _description = "Part Model"
+    name = fields.Char('name')
+    manufacturer = fields.Char('manufacturer')
+    price = fields.Float('price')
