0df45e5eb638e76f205df17ccacf8073ef706534
KaibaKudo <adrian.cortes.4869@gmail.com>
2023-02-15 10:30:33 +0100

    relaciones, forms e imagen en formulario hechos

--- odoo/addons/armas/models/models.py
+++ odoo/addons/armas/models/models.py
@@ -24,43 +24,56 @@ class Armas(models.Model):
     _name = 'ac.armas.categoria'
     _description = 'Armas'
     
-    nombre_arma = fields.Char('nombre_arma')
-    precio = fields.Float('precio')
-    referencia = fields.Char('referencia')
+    name = fields.Char('Nombre del arma:')
+    precio = fields.Float('Precio:')
+    referencia = fields.Char('Referencia:')
     categoria = fields.Selection([
         ('1', 'Fusil de asalto'),('2', 'Subfusil'),('3','Escopeta'),('4','Francotirador'),('5','Lanzacohetes')
     ], string='categoria')
-    disponibilidad = fields.Boolean('disponobilidad')
+    disponibilidad = fields.Boolean('Disponibilidad:')
+    imagen = fields.Image('Imagen del arma', store=True, relation="res.partner")
+    camuflaje_id = fields.Many2one('ac.camuflajes.armas', string='Camuflaje')
+    accesorios_id = fields.Many2one('ac.accesorios.armas', string='Accesorios')
+    clientes_ids = fields.Many2many('ac.clientes.tienda', string='Cliente')
+    
+    
+    
 
 
 class accesoriosArmas(models.Model):
     _name = 'ac.accesorios.armas'
     _description = 'AccesoriosArmas'
 
-    nombre_accesorio = fields.Char('nombre_accesorio')
-    precio = fields.Float('precio')
-    referencia = fields.Char('referencia')
-    disponibilidad = fields.Boolean('disponobilidad')
+    name = fields.Char('Nombre del accesorio:')
+    precio = fields.Float('Precio:')
+    referencia = fields.Char('Referencia:')
+    disponibilidad = fields.Boolean('Disponibilidad:')
+    accesorios_ids = fields.One2many('ac.armas.categoria', 'accesorios_id', string='Accesorios')
+    
+    
 
 
 class camuflajesArmas(models.Model):
     _name = 'ac.camuflajes.armas'
     _description = 'CamuflajesArmas'
 
-    nombre_camuflaje = fields.Char('nombre_camuflaje')
-    precio = fields.Float('precio')
-    referencia = fields.Char('referencia')
-    disponibilidad = fields.Boolean('disponobilidad')
+    name = fields.Char('Nombre del camuflaje:')
+    precio = fields.Float('Precio:')
+    referencia = fields.Char('Referencia:')
+    disponibilidad = fields.Boolean('Disponibilidad:')
+    camuflajes_ids = fields.One2many('ac.armas.categoria', 'camuflaje_id', string='Camuflajes')
+    
 
 
 class clientes(models.Model):
     _name = 'ac.clientes.tienda'
     _description = 'Clientes'
 
-    nombre_cliente = fields.Char('nombre_cliente')
-    telefono = fields.Integer('telefono')
-    fecha_nacimiento = fields.Date('fecha_nacimiento')
-    direccion = fields.Char('direccion')
+    name = fields.Char('Nombre del cliente:')
+    telefono = fields.Integer('Teléfono:')
+    fecha_nacimiento = fields.Date('Fecha de nacimiento:')
+    direccion = fields.Char('Dirección:')
+    arma_ids = fields.Many2many('ac.armas.categoria', string='Arma')
         
         
         
