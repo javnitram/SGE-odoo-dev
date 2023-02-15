135f0ed8c380f7bbde052ed211e7d376cae7bb2d
Berto99 <albertonaval@live.com>
2023-02-15 10:45:05 +0100

    vista formulario, relaciones tablas, imgProductos

--- odoo/addons/gimnasio/models/models.py
+++ odoo/addons/gimnasio/models/models.py
@@ -26,37 +26,42 @@ class ClientesGimnasio(models.Model):
     _description = 'ar.gimnasio.clientes'
 
     dni = fields.Char('DNI',required=True)
-    nombre = fields.Char('Nombre',required=True)
+    name = fields.Char('Nombre',required=True)
     activo = fields.Boolean('Activo?',default=True)
     telefono = fields.Integer('Telefono',required=True)
-
+    clases_ids = fields.Many2many('ar.gimnasio.clases', string='Clases')
+    inscripciones_id = fields.Many2one('ar.gimnasio.inscripciones', string='Inscripcion')
 
 class ClasesGimnasio(models.Model):
     _name = 'ar.gimnasio.clases'
     _description = 'ar.gimnasio.clases'
     
-    nombre_clase = fields.Char('Nombre_clase',required=True)
+    name = fields.Char('Nombre_clase',required=True)
     precio = fields.Float('Precio',required=True)
     monitor = fields.Char('Monitor',required=True)
+
+    clientes_ids = fields.Many2many('ar.gimnasio.clientes', string='Clientes')
     
 class InscripcionesGimnasio(models.Model):
     _name = 'ar.gimnasio.inscripciones'
     _description = 'ar.gimnasio.inscripciones'
 
-    tipo_inscripcion = fields.Selection([
-        ('0', '1 mes'),
-        ('1', '3 meses'),
-        ('2', '6 meses'),
-        ('3', '12 meses')
+    name = fields.Selection([
+        ('1 mes', '1 mes'),
+        ('3 meses', '3 meses'),
+        ('6 meses', '6 meses'),
+        ('12 meses', '12 meses')
     ],string='tipo_inscripcion')
     fecha_inscripcion = fields.Date('Fecha de inscripcion',required=True)
     precio = fields.Float('Precio')
+    clientesInscripcion_ids = fields.One2many('ar.gimnasio.clientes', 'inscripciones_id', string='Clientes_Inscripcion')
 
 class ProductosGimnasio(models.Model):
     _name = 'ar.gimnasio.productos'
     _description = 'ar.gimnasio.productos'
 
-    nombre_producto = fields.Char('Nombre del producto',required=True)
+    name = fields.Char('Nombre del producto',required=True)
     precio = fields.Float('Precio',required=True)
+    imagen = fields.Image('Imagen',store=True,relation="res.partner")
     disponibilidad = fields.Boolean('Disponibilidad',default=True)
     descripcion = fields.Char('Descripcion')
