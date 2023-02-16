fb80b21447240058f57195f8cd941ee067822846
fdezhector <hectorfernandezruiz.02@gmail.com>
2023-02-16 10:27:10 +0100

    requisitos mínimos

--- odoo/addons/floristeria/models/models.py
+++ odoo/addons/floristeria/models/models.py
@@ -2,52 +2,78 @@
 
 from odoo import models, fields, api
 
-class floristeria(models.Model):
-    _name = 'hf_floristeria_floristeria'
-    _description = 'Floristeria'
+class manager(models.Model):
+    _name = 'hf_floristeria_manager'
+    _description = 'Encargados'
     name = fields.Char('Nombre')
+    bouquet_ids = fields.Many2many('hf_floristeria_bouquet', string='Ramos de flores')
+    flower_pot_ids = fields.Many2many('hf_floristeria_flower_pot', string='Macetas')
 
 class flower(models.Model):
     _name = 'hf_floristeria_flower'
-    _description = 'Flor'
+    _description = 'Flores'
     name = fields.Char(string='Nombre común',required=True, help='')
+    bouquet_ids = fields.Many2many('hf_floristeria_bouquet', string=' Ramo de flores')
+    flower_pot_ids = fields.Many2many('hf_floristeria_flower_pot', string=' Maceta')
+    specie_id = fields.Many2one('hf_floristeria_specie', string='Especie', required=True)
+
     scientific_name = fields.Char(string='Nombre científico', required=True, help='')
-    specie = fields.Char(string='Especie', required=True, help='')
     main_color = fields.Char(string='Color predominante', help='')
-    group = fields.Selection([
-        ('0', ''),
-        ('A', 'Angiospermas'),
-        ('G', 'Gimnospermas')
-    ], string='Grupo',required=True)
     subgroup_gim = fields.Selection([
-        ('0', 'Cícadas'),
-        ('1', 'Ginko'),
-        ('2', 'Coníferas'),
-        ('3', 'Gnetales')
+        ('0', ''),
+        ('1', 'Cícadas'),
+        ('2', 'Ginko'),
+        ('3', 'Coníferas'),
+        ('4', 'Gnetales')
     ], string='Subgrupo de las gimnospermas',required=True)
     subgroup_ang = fields.Selection([
-        ('0', 'Amborellales'),
-        ('1', 'Nymphaeales'),
-        ('2', 'Austrobaileyales'),
-        ('3', 'Chloranthales'),
-        ('4', 'Magnólidas'),
-        ('5', 'Ceratophyllales'),
-        ('6', 'Eudicotiledóneas'),
-        ('7', 'Monocotiledóneas')
+        ('0', ''),
+        ('1', 'Amborellales'),
+        ('2', 'Nymphaeales'),
+        ('3', 'Austrobaileyales'),
+        ('4', 'Chloranthales'),
+        ('5', 'Magnólidas'),
+        ('6', 'Ceratophyllales'),
+        ('7', 'Eudicotiledóneas'),
+        ('8', 'Monocotiledóneas')
     ], string='Subgrupo de las angiospermas',required=True)
     flower_image = fields.Image('Imagen')
 
+class specie(models.Model):
+    _name = 'hf_floristeria_specie'
+    _description = 'Especies'
+    name = fields.Selection([
+        ('Gimnospermas', 'Gimnospermas'),
+        ('Angiospermas', 'Angiospermas')
+    ], string='Especie', required=True)
+    flower_ids = fields.One2many('hf_floristeria_flower', 'specie_id', string=' Flor')
+    
 class bouquet(models.Model):
     _name = 'hf_floristeria_bouquet'
     _description = 'Ramo de flores'
     name = fields.Char('Nombre', required=True)
+    flower_ids = fields.Many2many('hf_floristeria_flower', string=' Flor')
+    manager_ids = fields.Many2many('hf_floristeria_manager', string=' Encargados')
+    
     size = fields.Selection([
         ('0', ''),
         ('1', 'Pequeño'),
         ('2', 'Normal'),
-        ('3', 'Grande'),
-        ('4', 'Enorme')
+        ('3', 'Grande')
     ], string='Tamaño',required=True)
     price = fields.Float('Precio', required=True)
-    delivery_date = fields.Date('Fecha de entrega')
     bouquet_image = fields.Image('Imagen')
+
+class flower_pot(models.Model):
+    _name = 'hf_floristeria_flower_pot'
+    _description = 'Macetas'
+    name = fields.Char('Nombre', required=True)
+    flower_ids = fields.Many2many('hf_floristeria_flower', string=' Flor')
+    manager_ids = fields.Many2many('hf_floristeria_manager', string=' Encargados')
+    
+    size = fields.Selection([
+        ('0', ''),
+        ('1', 'Pequeño'),
+        ('2', 'Normal'),
+        ('3', 'Grande')
+    ], string='Tamaño',required=True)
