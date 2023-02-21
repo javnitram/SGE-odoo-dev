7060cbf10419c9286bffabcd45d36e76f71e2613
deleseee <deleseee@gmail.com>
2023-02-21 13:02:34 +0100

    Funcional con relacciones

--- odoo/addons/concesionario/models/models.py
+++ odoo/addons/concesionario/models/models.py
@@ -2,38 +2,42 @@
 
 from odoo import models, fields, api
 
-
 class ConcesionarioPedidos(models.Model):
     _name = 'dhm.concesionario.pedidos'
     _description = 'Marcas'
-    nombre = fields.Char('Nombre',required=True)
+    name = fields.Char('Nombre',required=True)
     descripcion = fields.Char('Descripcion')
-    vehiculos_ids = fields.One2many('dhm.concesionario.vehiculos', 'vehiculos_id', string='vehiculos')
+    facturas_id = fields.Many2one('dhm.concesionario.facturas', string='Facturas')
 
 class ConcesionarioVehiculos(models.Model):
     _name = 'dhm.concesionario.vehiculos'
     _description = 'Vehiculo'
-    marca = fields.Char('Nombre',required=True,help='Introduzca el nombre de la marca')
+    name=fields.Char('Marca',required=True,help='Introduzca el nombre de la marca')
     modelo = fields.Char('Nombre',required=True,help='Introduzca el nombre del modelo')
     estado = fields.Selection([
         ('0', 'Nuevo'),
         ('1', 'KM-0'),
-        ('2', 'Seminuevo')
+        ('2', 'Seminuevo'),
+        ('3', 'Alquilado')
     ], string='Estado')
+    imagen = fields.Image('imagen')
     matricula = fields.Integer('Matricula',required=True)
-    vehiculos_id = fields.Many2one('dhm.concesionario.vehiculos', string='vehiculos')
+    clientes_ids = fields.Many2many('dhm.concesionario.clientes', string='Clientes')
+
 
 class ConcesionarioClientes(models.Model):
     _name = 'dhm.concesionario.clientes'
     _description = 'Clientes'
-    dni = fields.Integer('Dni',required=True)
+    name = fields.Integer('Dni',required=True)
     nombre = fields.Char('Nombre',required=True,help='Introduzca el nombre')
     apellidos = fields.Char('Apellidos',required=True,help='Introduzca el apellido')
+    vehiculos_ids = fields.Many2many('dhm.concesionario.vehiculos', string='Vehiculos')
 
 
 class ConcesionarioFacturas(models.Model):
     _name = 'dhm.concesionario.facturas'
     _description = 'Facturas'
+    name = fields.Integer('Factura',required=True)
     cantidad = fields.Integer('Cantidad',required=True)
     precio = fields.Integer('Precio',required=True)
-    
+    pedidos_ids = fields.One2many('dhm.concesionario.pedidos', 'facturas_id', string='Pedidos')    
