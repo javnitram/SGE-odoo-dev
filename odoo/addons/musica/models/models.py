c84fd5550df0d227e195587fe11d76c8b1515fb2
jrubio02 <jrubiosantiago@gmail.com>
2023-02-23 11:06:28 +0100

    update(falta img en form)

--- odoo/addons/musica/models/models.py
+++ odoo/addons/musica/models/models.py
@@ -20,18 +20,15 @@ class TiendainstrumentosInstrumento(models.Model):
     segundamano = fields.Boolean('Segunda mano')
     estado = fields.Selection([
         ('0', 'Recien Fabricado'),('1', 'Casi Nuevo'), ('2', 'Algo Desgastado')
-    ], string='estado', default = "0")
-
+    ], string='Estado', default = "0")
+    ventas_ids = fields.Many2many('jr.tiendainstrumento.venta', string='Instrumentos vendidos')
     categoria_id = fields.Many2one('jr.tiendainstrumento.categoria', string='Categoria')
-    #ventas_ids = fields.Many2many('jr.tiendainstrumento.venta', string='Ventas')
-
+    
 
 class TiendainstrumentosVenta(models.Model):
     _name = 'jr.tiendainstrumento.venta'
     _description = 'model.technical.name'
     fecha = fields.Date('fecha')
-    #cliente_id = fields.Many2one('jr.tiendainstrumento.cliente', string='Cliente')
-    #clientes_ids = fields.Many2many('jr.tiendainstrumento.cliente','ventas_ids', string='Clientes')
     instrumentos_ids = fields.Many2many('jr.tiendainstrumento.instrumento', string='Instrumentos')
     cliente_id = fields.Many2one('jr.tiendainstrumento.cliente', string='Cliente')
 
@@ -39,27 +36,9 @@ class TiendainstrumentosVenta(models.Model):
 class TiendainstrumentosCliente(models.Model):
     _name = 'jr.tiendainstrumento.cliente'
     _description = 'model.technical.name'
-    name = fields.Char('Nombre', required=True)
+    name = fields.Char('Nombre y Apellidos', required=True)
     dni = fields.Char('dni', required=True)
     fecha_nacimiento = fields.Date('Fecha de nacimiento')
     phone = fields.Integer('Phone')
     ventas_ids = fields.One2many('jr.tiendainstrumento.venta', 'cliente_id', string='Compras')
-    #ventas_ids = fields.One2many('jr.tiendainstrumento.venta', 'cliente_id', string='Pedidos realizados')
-    #ventas_ids = fields.Many2many('jr.tiendainstrumento.venta', 'clientes_ids',  string='Ventas')
-   
-#oofon
-
-
-# class musica(models.Model):
-#     _name = 'musica.musica'
-#     _description = 'musica.musica'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    
