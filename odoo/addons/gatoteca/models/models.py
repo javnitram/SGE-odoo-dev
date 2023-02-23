4a34b505d34c9bdc89f033cd7818d57ea4e19477
Syna02 <suamytorres02@gmail.com>
2023-02-23 09:57:38 +0100

    finalizadoV2

--- odoo/addons/gatoteca/models/models.py
+++ odoo/addons/gatoteca/models/models.py
@@ -3,7 +3,7 @@
 from odoo import models, fields, api
 
 class GatotecaEmpleado(models.Model):
-    _name = 'gatoteca.empleado'
+    _name = 'ss.gatoteca.empleado'
     _description = 'Empleado'
     name = fields.Char('Empleado',required=True)
     sueldo = fields.Integer('Sueldo')
@@ -13,34 +13,34 @@ class GatotecaEmpleado(models.Model):
         ('1','Camarero')
     ], string='Puesto')
 
-    gatos_ids = fields.Many2many('gatoteca.gato', string='Gatos asignados')
+    gatos_ids = fields.Many2many('ss.gatoteca.gato', string='Gatos asignados')
 
 class GatotecaCliente(models.Model):
-    _name = 'gatoteca.cliente'
+    _name = 'ss.gatoteca.cliente'
     _description = 'Cliente'
     name = fields.Char('Cliente',required=True)
     email = fields.Char('Email')
     adopcion = fields.Boolean('Quiere adoptar')
 
-    gatos_ids = fields.One2many('gatoteca.gato', 'cliente_id', string='Gatos adoptados')
+    gatos_ids = fields.One2many('ss.gatoteca.gato', 'cliente_id', string='Gatos adoptados')
 
 class GatotecaGato(models.Model):
-    _name = 'gatoteca.gato'
+    _name = 'ss.gatoteca.gato'
     _description = 'Gato'
     name = fields.Char('Nombre',required=True)
     imagen = fields.Image('Imagen del gato', max_width=200, max_height=200)
 
-    empleados_ids = fields.Many2many('gatoteca.empleado', string='Empleados asignados')
-    cliente_id = fields.Many2one('gatoteca.cliente', string='Adoptado por')
-    raza_id = fields.Many2one('gatoteca.raza', string='Raza')
+    empleados_ids = fields.Many2many('ss.gatoteca.empleado', string='Empleados asignados')
+    cliente_id = fields.Many2one('ss.gatoteca.cliente', string='Adoptado por')
+    raza_id = fields.Many2one('ss.gatoteca.raza', string='Raza')
 
 class GatotecaRaza(models.Model):
-    _name = 'gatoteca.raza'
+    _name = 'ss.gatoteca.raza'
     _description = 'Registro de razas de gatos'
     name = fields.Char('Raza')
     procedencia = fields.Char('Procedencia')
 
-    gatos_ids = fields.One2many('gatoteca.gato', 'raza_id', string='Gatos')
+    gatos_ids = fields.One2many('ss.gatoteca.gato', 'raza_id', string='Gatos')
 
 
 # class gatoteca(models.Model):
