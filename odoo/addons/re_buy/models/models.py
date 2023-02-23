1426f352ac2910f1d40e5253f6f7db75b38685d4
WonderAlice <cuddlealice@gmail.com>
2023-02-23 21:42:33 +0100

    Added sold products, improved models and added more demo data.

--- odoo/addons/re_buy/models/models.py
+++ odoo/addons/re_buy/models/models.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import date
 
 
 class rebuyEmployees(models.Model):
     _name = 'am.rebuy.employees'
     name = fields.Char('Name')
-    employee_product_sold = fields.One2many('am.rebuy.stock', 'product_employee', string='Products sold')
+    employee_product_sold = fields.One2many('am.rebuy.stock_sold', 'product_employee', string='Products sold')
+    employee_product_bought = fields.One2many('am.rebuy.client_products', 'client_product_employee', string='Products bought')
     
 class rebuyProducts(models.Model):
     _name = 'am.rebuy.products'
@@ -14,8 +16,9 @@ class rebuyProducts(models.Model):
     name = fields.Char('Name', help = "Insert product name")
     product_image = fields.Image('Image')
     stock_ids = fields.One2many('am.rebuy.stock', 'product_id', string='Stock')
-    client_ids = fields.Many2many('am.rebuy.clients', string='Pre-order client')
+    sold_ids = fields.One2many('am.rebuy.stock_sold', 'product_id', string='Sold')
 
+    
 class rebuyStock(models.Model):
     _name = 'am.rebuy.stock'
     product_id = fields.Many2one('am.rebuy.products', string='Product')
@@ -24,14 +27,55 @@ class rebuyStock(models.Model):
     product_grade = fields.Selection([('a', 'A'), ('b', 'B'), ('c', 'C')], string='Grade')
     product_employee = fields.Many2one('am.rebuy.employees', string='Seller')
     product_client = fields.Many2one('am.rebuy.clients', string='Buyer')
+    product_reserved = fields.Many2many('am.rebuy.clients', string='Reservation')
+
+    def sell_stock(self):
+        for record in self:
+            product_obj = self.env['am.rebuy.stock_sold']
+            product_obj_delete = self.env['am.rebuy.stock']
+            defaults = {
+                'product_id': record.product_id.id,
+                'product_price': record.product_price,
+                'product_stock': record.product_stock,
+                'product_grade': record.product_grade,
+                'product_employee': record.product_employee.id,
+                'product_client': record.product_client.id,
+                'product_sold_date': date.today(),
+            }
+            product_obj.create(defaults)
+            product_obj_delete.search([('id','=', self.id)]).unlink()
+
+class rebuyStockSold(models.Model):
+    _inherit = 'am.rebuy.stock'
+    _name = 'am.rebuy.stock_sold'
+    product_id = fields.Many2one('am.rebuy.products', string='Product')
+    product_price = fields.Float('Price', help = "Insert product price")
+    product_stock = fields.Integer('Stock', help = "Insert product stock")
+    product_grade = fields.Selection([('a', 'A'), ('b', 'B'), ('c', 'C')], string='Grade')
+    product_employee = fields.Many2one('am.rebuy.employees', string='Seller')
+    product_client = fields.Many2one('am.rebuy.clients', string='Buyer')
+    product_sold_date = fields.Date('Selling date')
+
+    def return_stock(self):
+        for record in self:
+            product_obj = self.env['am.rebuy.stock']
+            product_obj_delete = self.env['am.rebuy.stock_sold']
+            defaults = {
+                'product_id': record.product_id.id,
+                'product_price': record.product_price,
+                'product_stock': record.product_stock,
+                'product_grade': record.product_grade,
+            }
+            product_obj.create(defaults)
+            product_obj_delete.search([('id','=', self.id)]).unlink()
 
 class rebuyClients(models.Model):
     _name = 'am.rebuy.clients'
     name = fields.Char('Name')
     client_fidelity_number = fields.Char('Fidelity card number')
     client_product_sold = fields.One2many('am.rebuy.client_products', 'client_product_client', string='Sold products')
-    client_product_bought = fields.One2many('am.rebuy.stock', 'product_client', string='Bought products')
-    preorder_ids = fields.Many2many('am.rebuy.stock', string='Pre-orders')
+    client_product_bought = fields.One2many('am.rebuy.stock_sold', 'product_client', string='Bought products')
+    client_reservation = fields.Many2many('am.rebuy.stock', string='Reservations')
 
 class rebuyClientProducts(models.Model):
     _name = 'am.rebuy.client_products'
