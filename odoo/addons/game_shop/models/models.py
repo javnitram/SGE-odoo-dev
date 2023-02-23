2baf020d37d36eb0de1877e05a9d4eb7380c6f24
sanbricio <sanbriciorojas11@gmail.com>
2023-02-23 18:49:13 +0100

    Nombres cambiados requesitos

--- odoo/addons/game_shop/models/models.py
+++ odoo/addons/game_shop/models/models.py
@@ -4,16 +4,16 @@ from odoo import models, fields, api
 
 
 class game_shop_tienda(models.Model):
-    _name = 'game_shop.tienda'
+    _name = 'sb_game_shop.tienda'
     _description = 'Tienda de videojuegos'
     name= fields.Char(string="Nombre",required=True,help="Introduce el nombre de la tienda")
     ubicacion = fields.Char(string="Ubicación",required=True,help="Introduce la ubicación de la tienda")
     horario = fields.Char(string ="Horario",help="Introduce el horario de la tienda",default="10:00")
-    almacen_id = fields.Many2one('game_shop.almacen', string="Almacen de la tienda")
-    videojuegos_ids = fields.Many2many('game_shop.videojuego', string="Videojuegos disponibles")
+    almacen_id = fields.Many2one('sb_game_shop.almacen', string="Almacen de la tienda")
+    videojuegos_ids = fields.Many2many('sb_game_shop.videojuego', string="Videojuegos disponibles")
 
 class game_shop_videojuego(models.Model):
-    _name = 'game_shop.videojuego'
+    _name = 'sb_game_shop.videojuego'
     _description = 'Videojuego'
     name = fields.Char(string="Nombre",required=True,help="Introduce el nombre de el videojuego")
     pegi= fields.Integer(string="Pegi",required=True,default=3,help="Introduce la edad minima para poder jugar al juego")
@@ -23,8 +23,8 @@ class game_shop_videojuego(models.Model):
         ('0', 'Nuevo'),('1','Seminuevo'),('2','Usado')
     ], string='Estado', required=True,default="0")
     imagen = fields.Image(string="Imagen", max_width=640, max_height=480)
-    distribuidor_id = fields.Many2one('game_shop.distribuidor', string="Distribuidor del juego")
-    tiendas_ids = fields.Many2many('game_shop.tienda', string='Tiendas disponibles')
+    distribuidor_id = fields.Many2one('sb_game_shop.distribuidor', string="Distribuidor del juego")
+    tiendas_ids = fields.Many2many('sb_game_shop.tienda', string='Tiendas disponibles')
     precio_iva = fields.Float(string="Precio con IVA",help="Precio calculado con el IVA", compute="_precioIVA",store=True)
     @api.depends('precio','digital')
     def _precioIVA(self):
@@ -35,35 +35,17 @@ class game_shop_videojuego(models.Model):
                 i.precio_iva = i.precio*1.21
 
 class game_shop_distribuidor(models.Model):
-    _name = 'game_shop.distribuidor'
+    _name = 'sb_game_shop.distribuidor'
     _description = 'Distribuidor'
     name = fields.Char(string="Nombre", required=True,help="Introduce el nombre del distribuidor")
     pais_id = fields.Many2one('res.country', string='Pais')
     provincia_id = fields.Many2one('res.country.state', string='Provincia')
-    videojuegos_ids = fields.One2many('game_shop.videojuego', 'distribuidor_id', string="Juegos que son distribuidos")
+    videojuegos_ids = fields.One2many('sb_game_shop.videojuego', 'distribuidor_id', string="Juegos que son distribuidos")
 
 class game_shop_almacen(models.Model):
-    _name = 'game_shop.almacen'
+    _name = 'sb_game_shop.almacen'
     _description = 'Almacen juegos'
     name = fields.Char(string = "Almacen",required=True,help="Introduce el nombre del almacen",default="almacen")
     provincia_id = fields.Many2one('res.country.state', string='Provincia')
     calle = fields.Char(string="Calle", help="Introduce el nombre de la calle")
-    tiendas_ids = fields.One2many('game_shop.tienda', 'almacen_id', string="Tiendas a la que suminsitra")
-
-
-    
-
-
-# class game_shop(models.Model):
-#     _name = 'game_shop.game_shop'
-#     _description = 'game_shop.game_shop'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    tiendas_ids = fields.One2many('sb_game_shop.tienda', 'almacen_id', string="Tiendas a la que suminsitra")
