219c3c1974b4a95102f168b7c977cd030fad5565
AlonsoRial <alonsorivasalcobendas@gmail.com>
2023-02-23 12:40:10 +0100

    xd

--- odoo/addons/pokemodulo/models/models.py
+++ odoo/addons/pokemodulo/models/models.py
@@ -101,6 +101,52 @@ class pokemoduloEquipo2(models.Model):
 
 
 
+class pokemoduloEquipo3(models.Model):
+     _name = 'ar.pokemodulo.equipo3'
+     _description = 'pokemodulo_equipo3'
+     Imagen = fields.Image(string="Imagen",store=True,relation="res.partner",help="Seleccionar imagen aquí", required=True)
+     Nombre = fields.Char(string = 'Nombre de la Especie' ,required=True)
+     Mote = fields.Char('Mote del Pokemon')
+     Tipo1 = fields.Selection ([
+         ('0','Acero'),
+         ('1','Agua'),
+         ('2', 'Bicho'),
+         ('3', 'Dragon'),
+         ('4', 'Electrico'),
+         ('5', 'Fantasma'),
+         ('6', 'Fuego'),
+         ('7', 'Hada'),
+         ('8', 'Hielo'),
+         ('9', 'Lucha'),
+         ('10', 'Normal'),
+         ('11', 'Planta'),
+         ('12', 'Psiquico'),
+         ('13', 'Roca'),
+         ('14', 'Siniestro'),
+         ('15', 'Tierra'),
+         ('16', 'Veneno'),
+         ('17', 'Volador')
+     ],string = 'Tipo1', required=True)
+     Tipo2 = fields.Selection ([
+         ('0','Acero'),
+         ('1','Agua'),
+         ('2', 'Bicho'),
+         ('3', 'Dragon'),
+         ('4', 'Electrico'),
+         ('5', 'Fantasma'),
+         ('6', 'Fuego'),
+         ('7', 'Hada'),
+         ('8', 'Hielo'),
+         ('9', 'Lucha'),
+         ('10', 'Normal'),
+         ('11', 'Planta'),
+         ('12', 'Psiquico'),
+         ('13', 'Roca'),
+         ('14', 'Siniestro'),
+         ('15', 'Tierra'),
+         ('16', 'Veneno'),
+         ('17', 'Volador')
+     ],string = 'Tipo2')
 
 
 
