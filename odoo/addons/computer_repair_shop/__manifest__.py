c009f53c9d8b6ae746b78c9225c27c132e25149d
Arkidon <arkidon850@gmail.com>
2023-02-24 21:39:31 +0100

    Updated models and views

--- odoo/addons/computer_repair_shop/__manifest__.py
+++ odoo/addons/computer_repair_shop/__manifest__.py
@@ -27,6 +27,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'menu/menu.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- odoo/addons/computer_repair_shop/models/models.py
+++ odoo/addons/computer_repair_shop/models/models.py
@@ -10,38 +10,40 @@ class Users(models.Model):
     email = fields.Char('email')
 
 
-class Technician(models.Model):
-    _name = "ws.technician"
+class Technicians(models.Model):
+    _name = "ws.technicians"
     _description = "Technician Model"
 
     name = fields.Char('name')
     photo = fields.Image('photo', max_width=512, max_height=512)
 
 
-class Repairment(models.Model):
-    _name = 'ws.repairment'
+class Repairments(models.Model):
+    _name = 'ws.repairments'
     _description = 'Repair Model'
     repair_status = fields.Selection([
-        ('quote', 'Presupuesto'),
-        ('rejected', 'Rechazado'),
-        ('accepted', "Aceptado"),
-        ('processing', "En proceso"),
-        ('ready', 'Listo')
+        ('inspection_pending', 'Inspection pending'),
+        ('quote', 'Quote'),
+        ('rejected', 'Rejected'),
+        ('accepted', "Accepted"),
+        ('processing', "In process"),
+        ('ready', 'Ready to pickup'),
+        ('completed', 'Completed')
     ], string='repair_status')
     start_date = fields.Date('start_date')
     completion_date = fields.Date('completion_date')
     labor_cost = fields.Float('labor_cost')
 
 
-class Replacement(models.Model):
-    _name = "ws.replacement"
+class Replacements(models.Model):
+    _name = "ws.replacements"
     _description = "Replacement Model"
     old_part_id = fields.Many2one('comodel_name', string='old_part')
     new_part_id = fields.Many2one('comodel_name', string='new_part')
 
 
-class Part(models.Model):
-    _name = "ws.part"
+class Parts(models.Model):
+    _name = "ws.parts"
     _description = "Part Model"
     name = fields.Char('name')
     manufacturer = fields.Char('manufacturer')
