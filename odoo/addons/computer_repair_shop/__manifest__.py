9e13bd7e7ab46c27bfe40cfaeb44b2901905c004
Arkidon <arkidon850@gmail.com>
2023-02-25 18:59:31 +0100

    Updated module info

--- odoo/addons/computer_repair_shop/__manifest__.py
+++ odoo/addons/computer_repair_shop/__manifest__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "computer_repair_shop",
+    'name': "Computer Repair",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Computer repair shop management""",
 
     'description': """
-        Long description of module's purpose
+        A module to manage the repairments, computers, parts, clients and technicians
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "William Santana",
+    'website': "http://example.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -29,6 +28,7 @@
         'views/clients_views.xml',
         'views/motherboards_views.xml',
         'views/technicians_views.xml',
+        'views/computers_views.xml',
         'views/templates.xml',
         'menu/menu.xml',
     ],
