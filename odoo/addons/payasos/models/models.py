dd31bedc6b6c5698026c8384ae57c17f4298045c
robocob27 <gonzalezgarciadaniel2@gmail.com>
2023-02-25 16:40:16 +0100

    Modelos, vistas y seguridad hecho

--- odoo/addons/payasos/models/models.py
+++ odoo/addons/payasos/models/models.py
@@ -4,38 +4,53 @@ from odoo import models, fields, api
 
 
 class payasos(models.Model):
-    _name = 'dg.payasos'
-    _description = 'dg.payasos'
+    _name = 'dg.payasos.payasos'
+    _description = 'dg.payasos.payasos'
 
-    nombre = fields.Char('Nombre')
-    edad = fields.Integer('Edad')
-    color_nariz = fields.Char('Color de nariz')
-    tarifa = fields.Integer('tarifa')
+    name = fields.Char('Nombre', required=True)
+    description = fields.Text('Descripcion', help='Breve descripcion del payaso')
+    edad = fields.Integer('Edad', required=True)
+    color_nariz = fields.Selection([
+        ('0', 'rojo'),
+        ('1', 'verde'),
+        ('2', 'azul celeste'),
+        ('3', 'morado'),
+        ('4', 'amarillo'),
+        ('5', 'naranja'),
+    ], string='Color de nariz', required=True, default=0)
+    tarifa = fields.Float('tarifa', required=True)
     #id_manager = fields.Many2one('dg.managers', string='id')
 
 class managers(models.Model):
-    _name = 'dg.managers'
-    _description = 'dg.managers'
+    _name = 'dg.payasos.managers'
+    _description = 'dg.payasos.managers'
 
-    nombre = fields.Char('Nombre')
-    annos = fields.Integer('Años en la empresa')
+    name = fields.Char('Nombre', required=True)
+    annos = fields.Integer('Años en la empresa', required=True)
 
 class fiestas(models.Model):
-    _name = 'dg.fiestas'
-    _description = 'dg.fiestas'
+    _name = 'dg.payasos.fiestas'
+    _description = 'dg.payasos.fiestas'
 
-    fecha = fields.Date('Fecha')
-    direccion = fields.Char('Dirección')
-    hora_comienzo = fields.Float('Hora de comienzo', compute="_compute_time")
-    hora_fin = fields.Float('Hora de fin', compute="_compute_time")
+    description = fields.Text('Tipo de fiesta', help='Cumpleaños, fin de curso...', required=True)
+    fecha = fields.Date('Fecha', required=True)
+    direccion = fields.Char('Dirección', required=True)
+    hora_comienzo = fields.Float('Hora de comienzo')
+    hora_fin = fields.Float('Hora de fin')
 
 class clientes(models.Model):
-    _name = 'dg.clientes'
-    _description = 'dg.clientes'
+    _name = 'dg.payasos.clientes'
+    _description = 'dg.payasos.clientes'
+    _sql_constraints = [
+                     ('dni_unique', 
+                      'unique(dni)',
+                      'ERROR: Ese DNI ya existe')
+    ]
 
-    nombre = fields.Char('Nombre')
-    edad = fields.Integer('Edad')
     dni = fields.Char('DNI', required=True)
+    name = fields.Char('Nombre', required=True)
+    edad = fields.Integer('Edad')
+    
 #
 #  @api.depends('value')
 #   def _value_pc(self):
