f58c71a56e92184d74140e590906870972ec8f3c
amsamu <samaropulido@gmail.com>
2023-02-25 20:51:44 +0100

    Manifest summary

--- odoo/addons/conciertos/__manifest__.py
+++ odoo/addons/conciertos/__manifest__.py
@@ -3,8 +3,7 @@
     'name': 'Conciertos',
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Módulo de gestión de conciertos, artistas, recintos y venta de entradas""",
 
     'description': """
         Long description of module's purpose
