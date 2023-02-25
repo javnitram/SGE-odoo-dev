c20666385b87efc44cc7f36c5d16c640d6f577c7
jaimedz <74jdfernandez@gmail.com>
2023-02-25 12:06:22 -0500

    Finalizado

--- odoo/addons/cine_modulo/__manifest__.py
+++ odoo/addons/cine_modulo/__manifest__.py
@@ -3,20 +3,21 @@
     'name': "Cine_modulo",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Modulo de cine.
+        """,
 
-    'description': """
-        Long description of module's purpose
+    'description': 
+    """
+       Módulo que se utiliza para seguir un registro de un cine con sus empleados, sus clientes, las peliculas disponibles y las distribuidoras
     """,
 
-    'author': "My Company",
+    'author': "Jaime Díaz Fenández",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'AOTY (Application Of The Year)',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -32,4 +33,5 @@
     'demo': [
         'demo/demo.xml',
     ],
+
 }
--- odoo/addons/cine_modulo/models/models.py
+++ odoo/addons/cine_modulo/models/models.py
@@ -2,7 +2,7 @@
 from odoo import models, fields, api
 
 class empleado(models.Model):
-    _name = 'cinema.empleado'
+    _name = 'jd_cinema.empleado'
     dni = fields.Char(string="DNI", required=True)
     nombre = fields.Char(string="Nombre", required=True)
     apellidos = fields.Char(string="Apellidos", required=True)
@@ -10,30 +10,34 @@ class empleado(models.Model):
     puesto = fields.Char(string="Puesto de trabajo")
 
 class cliente(models.Model):
-    _name = 'cinema.cliente'
+    _name = 'jd_cinema.cliente'
     name = fields.Integer(string  = "Nº Cliente", required = True)
     nombre = fields.Char(string = "Nombre", required = True)
     apellidos = fields.Char(string="Apellidos", required = True)
     telefono = fields.Integer(string = "Número de teléfono")
     cod_postal = fields.Integer(string = "Código postal")
+    empleado_ids = fields.Many2many("jd_cinema.empleado",string="Encargado de ventas")
+   
+    
        
 class peliculas(models.Model):
-    _name = 'cinema.pelicula'
+    _name = 'jd_cinema.pelicula'
     name = fields.Char(string  = "ID_pelicula", required = True)
     nombre = fields.Char(string = "Nombre", required = True)
     sinopsis = fields.Char(string="Sinopsis", required = True)
     duracion = fields.Integer(string = "Duracion")
     fecha_estreno = fields.Date(string = "Fecha de estreno")
-    distribuidora = fields.Many2one("cinema.distribuidora",string = "Distribuidora")
+    distribuidora = fields.Many2one("jd_cinema.distribuidora",string = "Distribuidora")
+    cartelera = fields.Image(string = "Cartelera")
 
 class distribuidora(models.Model):
-    _name = "cinema.distribuidora"
+    _name = "jd_cinema.distribuidora"
     name = fields.Char(string = "ID_distribuidora", required=True)
     nombre = fields.Char(string = "Nombre Distribuidora", required=True)
-    pelicula = fields.One2many("cinema.pelicula","distribuidora",string="Pelicula")
+    pelicula = fields.One2many("jd_cinema.pelicula","distribuidora",string="Pelicula")
 
 class sesion(models.Model):
-    _name = "cinema.sesion"
+    _name = "jd_cinema.sesion"
     numero_sala = fields.Integer(string = "Nº de sala")
-    pelicula = fields.Many2one("cinema.pelicula",string = "Pelicula")
-    fecha_sesion = fields.Date(string = "Fecha de la sesion, incluye hora")
+    pelicula = fields.Many2one("jd_cinema.pelicula",string = "Pelicula")
+    fecha_sesion = fields.Datetime(string = "Fecha de la sesion, incluye hora")
