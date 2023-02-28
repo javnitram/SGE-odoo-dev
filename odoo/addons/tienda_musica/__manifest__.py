1133ada05ddf420f522d215b7320dc0f4047b681
IvanRodriguez67 <alumno.ivan.rodriguez.1d@gmail.com>
2023-02-28 14:11:53 +0100

    Version Final1

--- odoo/addons/tienda_musica/__manifest__.py
+++ odoo/addons/tienda_musica/__manifest__.py
@@ -8,13 +8,13 @@
         Módulo para gestionar una tienda de música con distintas categorías, búsquedas por cantantes, discos, albums...
     """,
 
-    'author': "My Company",
+    'author': "Ivan Rodriguez",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'SG23',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
