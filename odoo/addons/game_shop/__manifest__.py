95dc24eeb5e6eda3785653a1eff5e33a8708864b
sanbricio <sanbriciorojas11@gmail.com>
2023-03-03 18:58:45 +0100

    Version 1.03(Kanban upgrade)

--- odoo/addons/game_shop/__manifest__.py
+++ odoo/addons/game_shop/__manifest__.py
@@ -16,13 +16,12 @@
     """,
 
     'author': "Santiago Bricio Rojas",
-    'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'SGE',
-    'version': '1.02',
+    'category': 'SGE23',
+    'version': '1.03',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
