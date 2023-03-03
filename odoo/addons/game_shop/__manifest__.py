c888f35b3d1f31fd57a90c56b68aadef073798ca
sanbricio <sanbriciorojas11@gmail.com>
2023-03-03 18:15:11 +0100

    1.02 Version(Graph view)

--- odoo/addons/game_shop/__manifest__.py
+++ odoo/addons/game_shop/__manifest__.py
@@ -21,8 +21,8 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Sales',
-    'version': '1.0',
+    'category': 'SGE',
+    'version': '1.02',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
