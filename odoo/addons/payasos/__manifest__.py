9284353aca7278ebd340ec59d9b8a1d0945e6bc5
robocob27 <gonzalezgarciadaniel2@gmail.com>
2023-03-08 16:47:02 +0100

    sql_constraints añadidas, manifest limpiado

--- odoo/addons/payasos/__manifest__.py
+++ odoo/addons/payasos/__manifest__.py
@@ -3,8 +3,8 @@
     'name': "payasos",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Payasos a domicilio
+    """,
 
     'description': """
         Gestion de una empresa de payasos a domicilio, con clientes y ubicaciones de fiestas.
@@ -13,22 +13,16 @@
     'author': "Daniel Gonzalez",
     'website': "http://www.yourcompany.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base'],
 
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [
        'demo/demo.xml',
     ],
--- odoo/addons/payasos/models/models.py
+++ odoo/addons/payasos/models/models.py
@@ -6,7 +6,15 @@ from odoo import models, fields, api
 class payasos(models.Model):
     _name = 'dg.payasos.payasos'
     _description = 'dg.payasos.payasos'
-
+    _sql_constraints = [
+                     ('tarifa_check', 
+                      'check(tarifa>0 AND tarifa<100)',
+                      'ERROR: La tarifa debe estar entre el rango de 0 y 100'),
+                      ('edad_check', 
+                      'check(edad>16)',
+                      'ERROR: El payaso debe tener edad legal para trabajar')
+    ]
+    
     name = fields.Char('Nombre', required=True)
     description = fields.Text('Descripcion', help='Breve descripcion del payaso')
     edad = fields.Integer('Edad', required=True)
@@ -21,18 +29,37 @@ class payasos(models.Model):
     tarifa = fields.Float('tarifa', required=True)
     manager_id = fields.Many2one('dg.payasos.managers', string='Manager')
     fiestas_ids = fields.Many2many('dg.payasos.fiestas', string='Fiestas asistidas')
+    
 
 class managers(models.Model):
     _name = 'dg.payasos.managers'
     _description = 'dg.payasos.managers'
-
+    _sql_constraints = [
+                     ('annos_check', 
+                      'check(NOT annos < 0)',
+                      'ERROR: Los años no pueden ser negativos')
+    ]
+    
     name = fields.Char('Nombre', required=True)
     annos = fields.Integer('Años en la empresa', required=True)
     foto = fields.Image(string="Foto",store=True,relation="res.partner",help="Agregar imagen")
     payasos_ids = fields.One2many('dg.payasos.payasos', 'manager_id', string='Payasos manejados')
+    
+
 class fiestas(models.Model):
     _name = 'dg.payasos.fiestas'
     _description = 'dg.payasos.fiestas'
+    _sql_constraints = [
+                     ('horasDia_check', 
+                      'check(hora_comienzo >=6 AND hora_fin < 24)',
+                      'ERROR: Nuestra empresa atiende fiestas dentro de nuestro horario: 6:00 AM - 23:59 PM'),
+                      ('horaReal_check', 
+                      'check(hora_comienzo < hora_fin)',
+                      'ERROR: La fiesta no puede acabar antes de que comience'),
+                      ('duracionMinima_check', 
+                      'check((hora_fin - hora_comienzo) >= 0.5)',
+                      'ERROR: La fiesta debe durar un mínimo de 30 minutos')
+    ]
 
     description = fields.Text('Tipo de fiesta', help='Cumpleaños, fin de curso...', required=True)
     fecha = fields.Date('Fecha', required=True)
@@ -41,6 +68,7 @@ class fiestas(models.Model):
     hora_fin = fields.Float('Hora de fin')
     cliente_id = fields.Many2one('dg.payasos.clientes', string='Organizador')
     payasos_ids = fields.Many2many('dg.payasos.payasos', string='Payasos asistentes', required=True)
+    
 
 class clientes(models.Model):
     _name = 'dg.payasos.clientes'
@@ -55,9 +83,3 @@ class clientes(models.Model):
     name = fields.Char('Nombre', required=True)
     edad = fields.Integer('Edad')
     fiestas_ids = fields.One2many('dg.payasos.fiestas', 'cliente_id', string='Fiestas organizadas')
-
-#
-#  @api.depends('value')
-#   def _value_pc(self):
-#        for record in self:
-#            record.value2 = float(record.value) / 100
