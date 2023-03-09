1b6dd926edb10bb45007285868c6f4e44c53e01d
ElFetouhy <ismailef85@gmail.com>
2023-03-09 07:33:57 +0100

    Ultimo commit

--- odoo/addons/concesionario/__manifest__.py
+++ odoo/addons/concesionario/__manifest__.py
@@ -3,15 +3,14 @@
     'name': "Concesionario",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Módulo concesionario""",
 
     'description': """
-        Long description of module's purpose
+        El módulo gestiona un concesionario  
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Fetouhy-28",
+    'website': "http://www.ismailelfetouhy.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
--- odoo/addons/concesionario/models/models.py
+++ odoo/addons/concesionario/models/models.py
@@ -2,42 +2,49 @@
 
 from odoo import models, fields, api
 
-
-# class concesionario(models.Model):
-#     _name = 'concesionario.concesionario'
-#     _description = 'concesionario.concesionario'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+#class EstadoCoches(models.Model):
+#    _name='concesionario.coches_kanban'
+#    _description = 'concesionario.coches_kanban'
+    
 
 class Coches(models.Model):
     _name = 'concesionario.coches'
     _description = 'concesionario.coches'
-    #matricula = fields.Char(string='matricula', required=True)
     marca = fields.Char(string='marca')
     modelo = fields.Char(string='modelo')
     cv = fields.Integer(string="Potencia(CV)")
-    color = fields.Char('color')
-    estado = fields.Char('estado')
-    matricula = fields.Many2one('concesionario.clientes', string='Matricula')
+    color = fields.Char(string="color")
+    estado = fields.Selection([
+        ('vendido', 'Vendido'),('alquilado','Alquilado'),('reparado','Reparado')
+    ], string='estado')
+    matricula = fields.Many2one('concesionario.clientes', string="matricula")
 
-class CochesAñadir(models.Model):
-    _name = 'concesionario.coches_annadir'
-    _description = 'concesionario.coches_annadir'
-    matricula = fields.Char(string='Matricula')
-    marca = fields.Char(string='Marca')
-    modelo = fields.Char(string='Modelo')
-    cv = fields.Integer(string="Potencia(CV)")
-    color = fields.Char('Cplor')
-    estado = fields.Char('Estado')
     
+    
+
+#class CocheCompradoVendido(models.Model):
+#    _name ='concesionario.coches_comprado_vendido'
+#    _description ='concesionario.coches_comprado_vendido'
+    #_inherit = ['concesionario.coches']
+    #@api.multi
+    #def _selecionarEstado(self):
+    #    self.ensure.one()
+    #    if self.estado
+
+# class CochesAñadir(models.Model):
+#     _name = 'concesionario.coches_annadir'
+#     _description = 'concesionario.coches_annadir'
+#     marca = fields.Char(string='Marca')
+#     modelo = fields.Char(string='Modelo')
+#     cv = fields.Integer(string="Potencia(CV)")
+#     color = fields.Char('Cplor')
+#     estado = fields.Selection([
+#         ('vendido', 'Vendido'),('alquilado','Alquilado'),('reparado','Reparado')
+#     ], string='estado')
+#     matricula = fields.Char(string="matricula")
+
+
+
 
 class Clientes(models.Model):
     _name = 'concesionario.clientes'
@@ -46,18 +53,8 @@ class Clientes(models.Model):
     apellidos = fields.Char(string="Apellidos" ,required=True)
     direccion = fields.Char(string="Dirección")
     telefono = fields.Integer(string="Teléfono")
-    matricula = fields.Char(string="Coche")
-    #matricula_ids = fields.One2many('concesionario.clientes', 'matricula', string='ID Matricula')
-    #field_name_ids = fields.One2many('comodel_name', 'inverse_field_name', string='field_name')
+    matricula = fields.Char(string="matricula")
     
-    
-#class Tipo(models.Model):
-#    _name = 'concesionario.tipo'
-#    _description = 'concesionario.tipo'
-#    idCoche = fields.Char('Matricula del coche' ,required=True)
-#    estado = fields.Char('Estado',required=True)
-#    fechaIn =  fields.Date('Fecha Inicial')
-#    fechaFin = fields.Date('Fecha Finalizacion') 
     
 class Empleados(models.Model):
     _name = 'concesionario.empleados'
@@ -66,13 +63,18 @@ class Empleados(models.Model):
     apellidos = fields.Char(string="Apellidos")
     nrovendidos = fields.Integer(String="Número de coches vendidos")
     salario = fields.Integer(string="Salario")
-    #<!--clientevendido = fields.Char('clientevendido')-->
+    
+    
 
 class Mecanicos(models.Model):
     _name = 'concesionario.mecanicos'
     _description = 'concesionario.mecanicos'
+    id_mecanico = fields
     nombre = fields.Char(string="Nombre",required=True)
     apellidos = fields.Char(string="Apellidos",required=True)
+    salario = fields.Integer(string="Salario")
+    numero_reparaciones = fields.Integer(string="Numero de repaciones")
+
 
 
     
